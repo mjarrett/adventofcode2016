cdc20c03e4155d9fb44e1060dcdbc357a0c2aa57
Mike Jarrett <msjarrett@gmail.com>
2016-12-01 22:25:40 -0800

    more complete solution, but part 1 isn't quite working???

--- day2.py
+++ day2.py
@@ -4,42 +4,18 @@ import numpy as np
 import re
 
 
+keypad1 = {(0,0):7,(1,0):8,(2,0):9,(0,1):4,(1,1):5,(1,2):6,(0,2):1,(1,2):2,(2,2):3}
+start1 = [1,1]
+keypad2 = {(2,0):'D',(1,1):'A',(2,1):'B',(3,1):'C',(0,2):5,(1,2):6,(2,2):7,(3,2):8,(4,2):9,(1,3):2,(2,3):3,(3,3):4,(2,4):1}
+start2 = [0,2]
 
-def part1():
+def solution(keypad,start):
     input = open('day2.txt')
     x = 1
     y = 1
     keys = []
-    keypad = {(0,0):7,(1,0):8,(2,0):9,(0,1):4,(1,1):5,(1,2):6,(0,2):1,(1,2):2,(2,2):3}
-    for line in input:
-        #print(line)
-        for step in line:
-            #print(step)
-            if step == 'U' and 0 <= y < 2:
-                y = y + 1
-            elif step == 'D' and 0 < y <= 2:
-                y = y - 1
-            elif step == 'R' and 0 <= x < 2:
-                x = x + 1
-            elif step == 'L' and 0 < x <= 2:
-                x = x - 1
-            #else:
-            #    print("No move")
-
-            #print(x,y)
-        keys.append(keypad[(x,y)])
-    return keys
-
-#print(part1())
-
-def part2():
-    input = open('day2.txt')
-    x = 1
-    y = 1
-    keys = []
-    keypad = {(2,0):'D',(1,1):'A',(2,1):'B',(3,1):'C',(0,2):5,(1,2):6,(2,2):7,(3,2):8,(4,2):9,(1,3):2,(2,3):3,(3,3):4,(2,4):1}
+    key=start
     print(keypad)
-    key = [0,2]
     for line in input:
         #print(line)
         for step in line:
@@ -58,8 +34,9 @@ def part2():
 
         #keys.append(list(keypad.keys())[list(keypad.values()).index(tuple(key))])
         keys.append(keypad[tuple(key)])
-        print("*****")
+        #print("*****")
 
 
     return keys
-print(part2())
+print(solution(keypad1,start1))
+print(solution(keypad2,start2))
