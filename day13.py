526a68ae897808351682a8b3f454ea7e74e5f556
Mike Jarrett <msjarrett@gmail.com>
2016-12-13 21:47:54 -0800

    day13 very inneficient solution, not sure if it finishes

--- day13.py
+++ day13.py
@@ -2,11 +2,15 @@
 import random
 
 data = 1358
-end = (31,39)
-checked = [] #spots that have been checked
-current = (1,1)
+#data = 10 #test
+end = [31,39]
+#end = [7,4] # test
+start = [1,1]
 
-def iswall(x,y):
+#check if a given tile is a wall
+def iswall(nextm):
+    x = nextm[0]
+    y = nextm[1]
     loc = x*x + 3*x + 2*x*y + y + y*y + data
     loc = "{0:b}".format(loc)
     if loc.count('1')%2 == 0:
@@ -14,34 +18,54 @@ def iswall(x,y):
     else: return True
 
 
-def isvalid(state):
-    x = state[0]
-    y = state[1]
-    if x < 0 or y < 0:
-        return False
-    elif (x,y) in checked:
-        return False
-    elif iswall(x,y):
-        #print('wall')
-        return False
-    else: return True
-
-def nextmove(current):
+# check if a given move is valid
+def nextmove(current,checked):
     #print('current {}'.format(current))
     xory = random.randint(0,1)
     forb = random.randint(0,1)*2-1
     nextm = list(current)
     nextm[xory] += forb
     #print('next {}'.format(nextm))
-    if isvalid(nextm) == False:
-        nextmove(current)
-    return nextm
+    if iswall(nextm)  or current[0] < 0 or current[1] < 0 or nextm in checked:
+        return nextmove(current,checked)
+    else: return nextm
+
+# check if there are any moves available
+def anymove(current,checked):
+    moves = [[current[0]+1,current[1]],[current[0]-1,current[1]],[current[0],current[1]+1],[current[0],current[1]-1]]
+    #print(moves)
+    anygood = False
+    for move in moves:
+        #print(anygood)
+        #print(move)
+        if iswall(move)  or move[0] < 0 or move[1] < 0 or move in checked:
+            anygood = False
+        else: return True
+    return anygood
+
+#print(nextmove([1,1],[]))
+#print(anymove([1,1],[[0,0],[0,1],[1,2]]))
+def one_path(current,allpaths):
+    checked = [] #spots that have been checked
+    while True:
+        #print(current)
+        #print(checked)
+        if current == end:
+            checked.append(current)
+            return True,len(checked)-1
+        if anymove(current,checked) == False:
+            #print('No moves from {}'.format(current))
+            #print('Path: {}'.format(checked))
+            checked.append(current)
+            return False,len(checked)-1
+
+        checked.append(current)
+        current = nextmove(current,checked)
 
 
-#walls (2,1),(1,0)
-checked.append(current)
-print(checked)
-current = nextmove(current)
-checked.append(current)
-print(checked)
-current = nextmove(current)
+while True:
+    allpaths = []
+    win,length = one_path(start,allpaths)
+
+    if win:
+        print(length)
