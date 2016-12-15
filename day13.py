79f4d7fc13c5a6001923aaffb2d5ec1b0119666b
Mike Jarrett <msjarrett@gmail.com>
2016-12-15 14:01:11 -0800

    search is now recursive, need to randomize next step order better

--- day13.py
+++ day13.py
@@ -17,19 +17,6 @@ def iswall(nextm):
         return False
     else: return True
 
-
-# check if a given move is valid
-def nextmove(current,checked):
-    #print('current {}'.format(current))
-    xory = random.randint(0,1)
-    forb = random.randint(0,1)*2-1
-    nextm = list(current)
-    nextm[xory] += forb
-    #print('next {}'.format(nextm))
-    if iswall(nextm)  or current[0] < 0 or current[1] < 0 or nextm in checked:
-        return nextmove(current,checked)
-    else: return nextm
-
 # check if there are any moves available
 def anymove(current,checked):
     moves = [[current[0]+1,current[1]],[current[0]-1,current[1]],[current[0],current[1]+1],[current[0],current[1]-1]]
@@ -43,29 +30,86 @@ def anymove(current,checked):
         else: return True
     return anygood
 
-#print(nextmove([1,1],[]))
-#print(anymove([1,1],[[0,0],[0,1],[1,2]]))
-def one_path(current,allpaths):
-    checked = [] #spots that have been checked
-    while True:
-        #print(current)
-        #print(checked)
-        if current == end:
-            checked.append(current)
-            return True,len(checked)-1
-        if anymove(current,checked) == False:
-            #print('No moves from {}'.format(current))
-            #print('Path: {}'.format(checked))
-            checked.append(current)
-            return False,len(checked)-1
-
-        checked.append(current)
-        current = nextmove(current,checked)
 
 
-while True:
-    allpaths = []
-    win,length = one_path(start,allpaths)
+# check if a given move is valid
+# def nextmove(current,checked):
+#     #print('current {}'.format(current))
+#     xory = random.randint(0,1)
+#     forb = random.randint(0,1)*2-1
+#     nextm = list(current)
+#     nextm[xory] += forb
+#     #print('next {}'.format(nextm))
+#     if iswall(nextm)  or current[0] < 0 or current[1] < 0 or nextm in checked:
+#         return nextmove(current,checked)
+#     else: return nextm
 
-    if win:
-        print(length)
+
+# def one_path(current,allpaths):
+#     checked = [] #spots that have been checked
+#     while True:
+#         #print(current)
+#         #print(checked)
+#         if current == end:
+#             checked.append(current)
+#             return True,len(checked)-1
+#         if anymove(current,checked) == False:
+#             #print('No moves from {}'.format(current))
+#             #print('Path: {}'.format(checked))
+#             checked.append(current)
+#             return False,len(checked)-1
+#
+#         checked.append(current)
+#         current = nextmove(current,checked)
+
+def search(current):
+    #print(current)
+    # end conditions:
+    if current == end:
+        return True
+    elif iswall(current):
+        return False
+    elif current in checked:
+        return False
+    elif current[0] < 0 or current[1] < 0:
+        return False
+
+    # if no end conditions met, current goes in checked
+    checked.append(current)
+
+    # explore neighbours recursively until and end condition is met. Respect walls
+    # randomize search order
+    xory = random.randint(0,1)
+    forb = random.randint(0,1)*2-1
+    next1 = list(current)
+    next2 = list(current)
+    next3 = list(current)
+    next4 = list(current)
+    next1[xory] = next1[xory]+forb
+    next2[xory] = next2[xory]-forb
+    next3[1-xory] = next3[1-xory]+forb
+    next4[1-xory] = next4[1-xory]-forb
+    if search(next1) or search(next2) or search(next3) or search(next4):
+        return True
+    return False
+
+results = []
+for i in range(10000):
+    checked = []
+    search([1,1])
+    #print(checked)
+    myset = set([tuple(x) for x in checked])
+    results.append(len(myset))
+
+print(min(results))
+
+
+
+
+
+# while True:
+#     allpaths = []
+#     win,length = one_path(start,allpaths)
+#
+#     if win:
+#         print(length)
