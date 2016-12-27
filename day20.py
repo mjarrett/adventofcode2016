b02816d8d08862cdac488668ff9450254f2cd4fc
Mike Jarrett <msjarrett@gmail.com>
2016-12-27 15:59:16 -0800

    day19 runs for small n, crashes on large n

--- day20.py
+++ day20.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-nums = [x for x in range(4294967295+1)]
+top = 9
+
+nums = [ x for x in range(top+1) ]
 #print(nums)
 with open('day20.txt') as f:
     for line in f:
@@ -9,7 +11,6 @@ with open('day20.txt') as f:
         amax = int(r[1])+1
         #print(nums)
         #print(amin,amax)
-
         nums[amin:amax] = [-1]*(amax-amin)
     nums = [ x for x in nums if x > -1]
-    print(min(nums))
+    print(nums)
